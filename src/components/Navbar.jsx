f1695e1a22b592a4af720c8d2e66f306dc1d81bd
Andrey-DC-Silva <andreysilva.pf004@academico.ifsul.edu.br>
2026-06-13 16:09:31 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,47 +14,47 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
+      <div className='navbar-content'>
+        <a className="nav-link" href="/dashboard">
+          Dashboard
+        </a>
 
-      <a className="nav-link" href="/dashboard">
-        Dashboard
-      </a>
+        <a className="nav-link" href="/perfil">
+          <FaUserCog /> Meu Perfil
+        </a>
 
-      <a className="nav-link" href="/perfil">
-        <FaUserCog /> Meu Perfil
-      </a>
+        {user?.role === 'ADMIN' && (
+          <>
+            <a className="nav-link" href="/pesquisadores">
+              <FaRegUser /> Pesquisadores
+            </a>
 
-      {user?.role === 'ADMIN' && (
-        <>
-          <a className="nav-link" href="/pesquisadores">
-            <FaRegUser /> Pesquisadores
-          </a>
+            <a className="nav-link" href="/usuarios">
+              <FaRegUser /> Usuários
+            </a>
+          </>
+        )}
 
-          <a className="nav-link" href="/usuarios">
-            <FaRegUser /> Usuários
-          </a>
-        </>
-      )}
+        <a className="nav-link" href="/experimentos">
+          <FaFlask /> Experimentos
+        </a>
 
-      <a className="nav-link" href="/experimentos">
-        <FaFlask /> Experimentos
-      </a>
+        <a className="nav-link" href="/projetos">
+          <FaProjectDiagram /> Projetos
+        </a>
 
-      <a className="nav-link" href="/projetos">
-        <FaProjectDiagram /> Projetos
-      </a>
+        <a className="nav-link" href="/relacionar">
+          <FaLink /> Relacionar
+        </a>
 
-      <a className="nav-link" href="/relacionar">
-        <FaLink /> Relacionar
-      </a>
+        <a className="nav-link" href="/relacionar2">
+          <FaLink /> Relacionar 2
+        </a>
 
-      <a className="nav-link" href="/relacionar2">
-        <FaLink /> Relacionar 2
-      </a>
-
-      <button className="logout-button" onClick={logout}>
-        <FaSignOutAlt /> Sair
-      </button>
-
+        <button className="logout-button" onClick={logout}>
+          <FaSignOutAlt /> Sair
+        </button>
+      </div>
     </nav>
   );
 }
